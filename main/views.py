803239b8f33846b2e33213ea458ee6e9cb1045f9
fpedaccio <fpedaccio@gmail.com>
2022-01-11 17:32:02 -0300

    V2

--- main/views.py
+++ main/views.py
@@ -16,17 +16,34 @@ def search_en(request):
     word = request.POST["wordInput"]
     response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')	
     data = response.json()
-    definition = data[0]["meanings"][0]["definitions"][0]["definition"]
-    example = data[0]["meanings"][0]["definitions"][0]["example"]
-    phonetic = data[0]["phonetic"]
-    audio = data[0]["phonetics"][0]["audio"]
+    try:
+        definition = data[0]["meanings"][0]["definitions"][0]["definition"]
+    except:
+        definition = "No definition found"
+    try:
+        example = data[0]["meanings"][0]["definitions"][0]["example"]
+    except:
+        example = "No example available"
+    try:
+        phonetic = data[0]["phonetic"]
+    except:
+        phonetic = "No phonetic available"
+    try:
+        audio = data[0]["phonetics"][0]["audio"]
+    except:
+        audio = "No audio available"
     return render(request, 'main/response_en.html', {'definition': definition, 'example': example, "word": word, "phonetic": phonetic,"audio": audio})
 
 def search_es(request):
     word = request.POST["wordInput"]
     response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/es/{word}')	
     data = response.json()
-    definition = data[0]["meanings"][0]["definitions"][0]["definition"]
-    example = data[0]["meanings"][0]["definitions"][0]["example"]
-
+    try:
+        definition = data[0]["meanings"][0]["definitions"][0]["definition"]
+    except:
+        definition = "No encontramos una definición a esta palabra"
+    try:
+        example = data[0]["meanings"][0]["definitions"][0]["example"]
+    except:
+        example = "No hay ejemplo disponible"
     return render(request, 'main/response_es.html', {'definition': definition, 'example': example, "word": word})
